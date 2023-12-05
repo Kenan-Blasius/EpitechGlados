5b3a0e3413f018e1e2966f25504d0d19743d4b55
Niels <91876336+NielsOuvrard@users.noreply.github.com>
2023-12-05 11:31:27 +0100

    ✨ remove useless comments

--- app/Main.hs
+++ app/Main.hs
@@ -8,18 +8,6 @@ import Parser
 -- INFO: Main function
 main :: IO ()
 main = do
---     -- Hardcoded test
---     let ast = IfAST (AST [SymbolAST ">=", SymbolAST "103", IntAST 103]) (AST [SymbolAST "True"]) (AST [IntAST 0])
---     putStrLn $ "AST: " ++ printAST ast
---     putStrLn $ "Result: " ++ printAST (evalAST ast)
-
-
--- Kenan's code
--- AST: IfAST (SymbolAST "=") (IntAST 0) (IntAST 2)
--- evalAST IfAST (SymbolAST "=") (IntAST 0) (IntAST 2)
--- evalAST SymbolAST "="
--- Symbol: =
--- Result: Nothing
     args <- getArgs
     case args of
         [filename] -> do
