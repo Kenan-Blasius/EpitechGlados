2a03a1e86aec10fe6c8fee8cfdde6e57b798810f
Niels <91876336+NielsOuvrard@users.noreply.github.com>
2024-01-04 10:09:15 +0100

    👍 better size of state int

--- app/BytecodeToBinary.hs
+++ app/BytecodeToBinary.hs
@@ -74,24 +74,47 @@ toHexaString x = map (fromIntegral . ord) x
 -- toHexaInstruction (Attribute x) = [0x10] ++ toHexaString x
 -- toHexaInstruction (CreateObject x) = [0x11] ++ toHexaInt x
 
+getLengthOfOperation :: Bytecode -> Int
+getLengthOfOperation (LoadConst _) = 2
+getLengthOfOperation (LoadVar _) = 2
+getLengthOfOperation (StoreVar _) = 2
+getLengthOfOperation (BinaryOp _) = 2
+getLengthOfOperation (UnaryOp _) = 2
+getLengthOfOperation (CompareOp _) = 2
+getLengthOfOperation (JumpIfTrue _) = 2
+getLengthOfOperation (JumpIfFalse _) = 2
+getLengthOfOperation (Jump _) = 2
+getLengthOfOperation Pop = 1
+getLengthOfOperation Dup = 1
+getLengthOfOperation (Call _) = 2
+getLengthOfOperation Return = 1
+getLengthOfOperation (BuildList _) = 2
+getLengthOfOperation Index = 1
+getLengthOfOperation (Attribute _) = 2
+getLengthOfOperation (CreateObject _) = 2
+
+
+
 toHexaInstruction :: Bytecode -> State Int [Word8]
-toHexaInstruction (LoadConst x) = trackBytes 1 >> return (0x01 : toHexaInt x)
-toHexaInstruction (LoadVar x) = trackBytes 1 >> return (0x02 : toHexaString x)
-toHexaInstruction (StoreVar x) = trackBytes 1 >> return (0x03 : toHexaString x)
-toHexaInstruction (BinaryOp x) = trackBytes 1 >> return (0x04 : toHexaString x)
-toHexaInstruction (UnaryOp x) = trackBytes 1 >> return (0x05 : toHexaString x)
-toHexaInstruction (CompareOp x) = trackBytes 1 >> return (0x06 : toHexaString x)
-toHexaInstruction (JumpIfTrue x) = trackBytes 1 >> return (0x07 : toHexaInt (x + 1))
-toHexaInstruction (JumpIfFalse x) = trackBytes 1 >> return (0x08 : toHexaInt (x + 1))
-toHexaInstruction (Jump x) = trackBytes 1 >> return (0x09 : toHexaInt (x + 1))
-toHexaInstruction Pop = trackBytes 1 >> return [0x0A]
-toHexaInstruction Dup = trackBytes 1 >> return [0x0B]
-toHexaInstruction (Call x) = trackBytes 1 >> return (0x0C : toHexaInt x)
-toHexaInstruction Return = trackBytes 1 >> return [0x0D]
-toHexaInstruction (BuildList x) = trackBytes 1 >> return (0x0E : toHexaInt x)
-toHexaInstruction Index = trackBytes 1 >> return [0x0F]
-toHexaInstruction (Attribute x) = trackBytes 1 >> return (0x10 : toHexaString x)
-toHexaInstruction (CreateObject x) = trackBytes 1 >> return (0x11 : toHexaInt x)
+toHexaInstruction (LoadConst x) =    trackBytes (getLengthOfOperation (LoadConst x))    >> return (0x01 : toHexaInt x)
+toHexaInstruction (LoadVar x) =      trackBytes (getLengthOfOperation (LoadVar x))      >> return (0x02 : toHexaString x)
+toHexaInstruction (StoreVar x) =     trackBytes (getLengthOfOperation (StoreVar x))     >> return (0x03 : toHexaString x)
+toHexaInstruction (BinaryOp x) =     trackBytes (getLengthOfOperation (BinaryOp x))     >> return (0x04 : toHexaString x)
+toHexaInstruction (UnaryOp x) =      trackBytes (getLengthOfOperation (UnaryOp x))      >> return (0x05 : toHexaString x)
+toHexaInstruction (CompareOp x) =    trackBytes (getLengthOfOperation (CompareOp x))    >> return (0x06 : toHexaString x)
+toHexaInstruction (JumpIfTrue x) =   trackBytes (getLengthOfOperation (JumpIfTrue x))   >> return (0x07 : toHexaInt (x + 1))
+toHexaInstruction (JumpIfFalse x) =  trackBytes (getLengthOfOperation (JumpIfFalse x))  >> return (0x08 : toHexaInt (x + 1)) -- TODO
+toHexaInstruction (Jump x) =         trackBytes (getLengthOfOperation (Jump x))         >> return (0x09 : toHexaInt (x + 1))
+toHexaInstruction Pop =              trackBytes (getLengthOfOperation Pop)              >> return [0x0A]
+toHexaInstruction Dup =              trackBytes (getLengthOfOperation Dup)              >> return [0x0B]
+toHexaInstruction (Call x) =         trackBytes (getLengthOfOperation (Call x))         >> return (0x0C : toHexaInt x)
+toHexaInstruction Return =           trackBytes (getLengthOfOperation Return)           >> return [0x0D]
+toHexaInstruction (BuildList x) =    trackBytes (getLengthOfOperation (BuildList x))    >> return (0x0E : toHexaInt x)
+toHexaInstruction Index =            trackBytes (getLengthOfOperation Index)            >> return [0x0F]
+toHexaInstruction (Attribute x) =    trackBytes (getLengthOfOperation (Attribute x))    >> return (0x10 : toHexaString x)
+toHexaInstruction (CreateObject x) = trackBytes (getLengthOfOperation (CreateObject x)) >> return (0x11 : toHexaInt x)
+    -- currentBytes <- get
+    -- trace ("Current bytes: " ++ show currentBytes ++ " x: " ++ show x ++ " (+ 2) = total: " ++ show (currentBytes + x + 2)) $                trackBytes (getLengthOfOperation x) >> return (0x08 : toHexaInt (currentBytes + x + 2))
 
 trackBytes :: Int -> State Int ()
 trackBytes n = modify (+ n)
@@ -122,5 +145,5 @@ writeBytesToFile bytes filePath = BS.writeFile filePath (BS.pack bytes)
 bytecodeToBinary :: [Bytecode] -> IO ()
 bytecodeToBinary bytecode = do
     let (bytes, totalBytes) = bytecodeToBytes bytecode
-    putStrLn $ "Total bytes written: " ++ show totalBytes
+    putStrLn $ "Total bytes written: " ++ show totalBytes ++ " bytes :" ++ show bytes
     writeBytesToFile bytes "file.bin"
