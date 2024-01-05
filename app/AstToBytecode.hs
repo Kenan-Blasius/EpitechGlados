9b7ae8810e7b159485554809eea075259afa35b4
Niels <91876336+NielsOuvrard@users.noreply.github.com>
2024-01-05 22:32:39 +0100

    🪃 code so much better, good usage of haskell

--- app/AstToBytecode.hs
+++ app/AstToBytecode.hs
@@ -90,53 +90,85 @@ valueSimpleToBytecode (AST (x:xs)) = trace ("valueSimpleToBytecode AST (x:xs): "
 valueSimpleToBytecode x = trace ("valueSimpleToBytecode NO AST SIMPLE NODE FOUND: " ++ show x) []
 
 
-exploreAllSymbols :: String -> Int -> [Bytecode]
-exploreAllSymbols "print" x = [LoadConst x, Call 1] -- system call 1 is write
-exploreAllSymbols "exit" x = [LoadConst x, Call 60] -- system call 60 is exit
-
-
-
 
 
 -- INFO: This function takes an AST and returns a list of Bytecode instructions
 --       that can be executed by the VM.
 astToBytecode' :: AST -> [Bytecode] -> (AST, [Bytecode])
 astToBytecode' (AST []) bytecode = (AST [], bytecode)
+
+-- * Define Int
+astToBytecode' (AST [IntTypeAST, SymbolAST x]) bytecode = trace ("AST [IntTypeAST, SymbolAST x]: " ++ show x) $ ((AST []), (bytecode ++ [StoreVar x]))
+
+-- ! only wait for AST ReturnAST
+astToBytecode' (AST [SymbolAST "return", x]) bytecode =
+    let (xAST, xBytecode) = astToBytecode' (AST [x]) bytecode
+    in (xAST, xBytecode ++ [Call 1]) -- system call 1 is write
+-- * System calls
+astToBytecode' (AST [SymbolAST "print", x]) bytecode =
+    let (xAST, xBytecode) = astToBytecode' (AST [x]) bytecode
+    in (xAST, xBytecode ++ [Call 1]) -- system call 1 is write
+astToBytecode' (AST [SymbolAST "exit", x]) bytecode =
+    let (xAST, xBytecode) = astToBytecode' (AST [x]) bytecode
+    in (xAST, xBytecode ++ [Call 60]) -- system call 60 is exit
+
+-- * (AST (x:xs))
 astToBytecode' (AST (x:xs)) bytecode = trace ("Processing AST node: " ++ show x) $
-    case x of
-        IntAST x -> trace ("IntAST: " ++ show x) $ astToBytecode' (AST xs) (bytecode ++ [LoadConst x])
-        (AST [SymbolAST "return", IntAST x']) -> trace ("return IntAST: " ++ show x') $ astToBytecode' (AST xs) (bytecode ++ [LoadConst x', Return]) -- ! will change t ReturnAST
-        IfAST cond (AST expr1) (AST elseIfExpr1) -> trace ("IfAST: " ++ show cond ++ " |expr1| " ++ show expr1 ++ " |do| " ++ show elseIfExpr1) $ do
-            let condBytecode = trace ("condBytecode1: " ++ show cond) astConditionToBytecode cond bytecode
-            let (expr1AST, expr1Bytecode) = trace ("expr1AST: " ++ show expr1) astToBytecode' (AST expr1) bytecode
-            let (elseIfExpr1AST, elseIfExpr1Bytecode) = trace ("elseIfExpr1: " ++ show elseIfExpr1 ++ "\n\n") astToBytecode' (AST elseIfExpr1) bytecode
-            (AST xs, bytecode ++ condBytecode ++ [JumpIfFalse (sizeInstructionOfAst (AST expr1) 0)] ++ expr1Bytecode ++ elseIfExpr1Bytecode)
-        ElseAST (AST expr1) -> trace ("ElseAST: " ++ show expr1) $ do
-            let (expr1AST, expr1Bytecode) = trace ("expr1AST: " ++ show expr1) astToBytecode' (AST expr1) bytecode
-            (AST xs, bytecode ++ expr1Bytecode)
+    let (xAST, xBytecode) = astToBytecode' x bytecode
+        (AST xsAST, xsBytecode) = astToBytecode' (AST xs) bytecode
+    in (AST xsAST, xBytecode ++ xsBytecode)
 
-        AssignAST (AST [IntTypeAST, SymbolAST x]) (AST [IntAST y]) ->
-            trace ("AssignAST: " ++ show x ++ " = " ++ show y)
-            $ astToBytecode' (AST xs) (bytecode ++ [LoadConst y, StoreVar x]) -- do this in several steps if several values to assign ( int a = 1 + 1 )
+astToBytecode' (IfAST cond (AST expr1) (AST elseIfExpr1)) bytecode = trace ("IfAST: " ++ show cond ++ " |expr1| " ++ show expr1 ++ " |do| " ++ show elseIfExpr1) $ do
+    let condBytecode = trace ("condBytecode1: " ++ show cond) astConditionToBytecode cond bytecode
+    let (expr1AST, expr1Bytecode) = trace ("expr1AST: " ++ show expr1) astToBytecode' (AST expr1) bytecode
+    let (elseIfExpr1AST, elseIfExpr1Bytecode) = trace ("elseIfExpr1: " ++ show elseIfExpr1 ++ "\n\n") astToBytecode' (AST elseIfExpr1) bytecode
+    (AST [], bytecode ++ condBytecode ++ [JumpIfFalse (sizeInstructionOfAst (AST expr1) 0)] ++ expr1Bytecode ++ elseIfExpr1Bytecode)
 
-        (AST [SymbolAST x, AST [IntAST y]]) ->
-            trace ("call exploreAllSymbols: " ++ show x ++ " = " ++ show y)
-            $ astToBytecode' (AST xs) (bytecode ++ exploreAllSymbols x y)
+-- ! check if good behaviour
+astToBytecode' (ElseAST (AST expr1)) bytecode = trace ("ElseAST: " ++ show expr1) $ do
+    let (expr1AST, expr1Bytecode) = trace ("expr1AST: " ++ show expr1) astToBytecode' (AST expr1) bytecode
+    (AST [], bytecode ++ expr1Bytecode)
 
-        -- SymbolAST x -> trace ("SymbolAST: " ++ show x) $ astToBytecode' (AST xs) (bytecode ++ [LoadVar x])
+-- * Assignation operation
+astToBytecode' (AssignAST x y) bytecode = trace ("AssignAST: " ++ show x ++ " = " ++ show y) $
+    let (AST yAST, yBytecode) = astToBytecode' y bytecode
+        (AST xAST, xBytecode) = astToBytecode' x bytecode
+    in (AST yAST, yBytecode ++ xBytecode)
 
-        _ -> trace ("AGAIN astToBytecode' (AST xs) bytecode" ++ show x) astToBytecode' (AST xs) bytecode
+-- * Simple operations
+astToBytecode' (PlusAST x y) bytecode = trace ("PlusAST: " ++ show x ++ " + " ++ show y) $
+    let (_, xBytecode) = astToBytecode' (AST [x]) bytecode
+        (_, yBytecode) = astToBytecode' (AST [y]) bytecode
+    in (AST [], concat [xBytecode, yBytecode, [BinaryOp "+"]])
+astToBytecode' (MinusAST x y) bytecode = trace ("MinusAST: " ++ show x ++ " - " ++ show y) $
+    let (_, xBytecode) = astToBytecode' (AST [x]) bytecode
+        (_, yBytecode) = astToBytecode' (AST [y]) bytecode
+    in (AST [], concat [xBytecode, yBytecode, [BinaryOp "-"]])
+astToBytecode' (TimesAST x y) bytecode = trace ("TimesAST: " ++ show x ++ " * " ++ show y) $
+    let (_, xBytecode) = astToBytecode' (AST [x]) bytecode
+        (_, yBytecode) = astToBytecode' (AST [y]) bytecode
+    in (AST [], concat [xBytecode, yBytecode, [BinaryOp "*"]])
+astToBytecode' (DivideAST x y) bytecode = trace ("DivideAST: " ++ show x ++ " / " ++ show y) $
+    let (_, xBytecode) = astToBytecode' (AST [x]) bytecode
+        (_, yBytecode) = astToBytecode' (AST [y]) bytecode
+    in (AST [], concat [xBytecode, yBytecode, [BinaryOp "/"]])
+astToBytecode' (ModuloAST x y) bytecode = trace ("ModuloAST: " ++ show x ++ " % " ++ show y) $
+    let (_, xBytecode) = astToBytecode' (AST [x]) bytecode
+        (_, yBytecode) = astToBytecode' (AST [y]) bytecode
+    in (AST [], concat [xBytecode, yBytecode, [BinaryOp "%"]])
 
-astToBytecode' (PlusAST (AST [IntAST x]) (AST [IntAST y])) bytecode = trace ("add PlusAST to the bytecode: " ++ show x ++ " + " ++ show y) $
-    astToBytecode' (AST []) (bytecode ++ [LoadConst (x) , LoadConst (y), BinaryOp "+"])
-astToBytecode' (MinusAST (AST [IntAST x]) (AST [IntAST y])) bytecode = trace ("MinusAST: " ++ show x ++ " - " ++ show y) $
-    astToBytecode' (AST []) (bytecode ++ [LoadConst (x) , LoadConst (y), BinaryOp "-"])
-astToBytecode' (TimesAST (AST [IntAST x]) (AST [IntAST y])) bytecode = trace ("TimesAST: " ++ show x ++ " * " ++ show y) $
-    astToBytecode' (AST []) (bytecode ++ [LoadConst (x) , LoadConst (y), BinaryOp "*"])
-astToBytecode' (DivideAST (AST [IntAST x]) (AST [IntAST y])) bytecode = trace ("DivideAST: " ++ show x ++ " / " ++ show y) $
-    astToBytecode' (AST []) (bytecode ++ [LoadConst (x) , LoadConst (y), BinaryOp "/"])
-astToBytecode' (ModuloAST (AST [IntAST x]) (AST [IntAST y])) bytecode = trace ("ModuloAST: " ++ show x ++ " % " ++ show y) $
-    astToBytecode' (AST []) (bytecode ++ [LoadConst (x) , LoadConst (y), BinaryOp "%"])
+-- * Load operations
+    -- [LoadConst x, Call 1] -- system call 1 is write
+-- astToBytecode' (AST [SymbolAST "exit", x]) bytecode = [LoadConst x, Call 60] -- system call 60 is exit
+-- TODO LoadVar
 astToBytecode' (SymbolAST x) bytecode = trace ("SymbolAST: " ++ show x) $ astToBytecode' (AST []) (bytecode ++ [LoadVar x])
+astToBytecode' (IntAST x) bytecode = trace ("IntAST: " ++ show x) $ astToBytecode' (AST []) (bytecode ++ [LoadConst x])
 
 astToBytecode' a b = trace ("Unknown AST node bytecode: " ++ show a ++ " " ++ show b) (a, b)
+-- LOAD_CONST 2
+-- LOAD_CONST 6
+-- BINARY_OP +
+-- LOAD_VAR a
+-- STORE_VAR []
+-- LOAD_VAR exit
+-- LOAD_CONST 10
