d964d4b66701d266398424db82093deba0c9f986
Niels <91876336+NielsOuvrard@users.noreply.github.com>
2024-01-07 12:04:47 +0100

    🌟 several bytes for int + fix old pc issue

--- app/BinaryEvaluation.hs
+++ app/BinaryEvaluation.hs
@@ -5,6 +5,8 @@ import Debug.Trace
 import qualified Data.ByteString.UTF8 as UTF8
 import Data.ByteString (unpack)
 import Data.Word (Word8)
+import Data.Bits
+import Data.List (genericTake)
 
 -- ; Opcode Definitions
 -- LOAD_CONST      0x01
@@ -21,19 +23,20 @@ import Data.Word (Word8)
 -- CALL            0x0C
 -- RETURN          0x0D
 
--- (LoadConst x)
--- (LoadVar x)
--- (StoreVar x)
--- (BinaryOp x)
--- (UnaryOp x)
--- (CompareOp x)
--- (JumpIfTrue x)
--- (JumpIfFalse x)
--- (Jump x)
--- Pop
--- Dup
--- (Call x)
--- Return
+-- (LoadConst x) -- 5
+-- (LoadVar x) -- 2
+-- (StoreVar x) -- 2
+-- (BinaryOp x) -- 2
+-- (UnaryOp x) -- 2
+-- (CompareOp x) -- 2
+-- (JumpIfTrue x) -- 5
+-- (JumpIfFalse x) -- 5
+-- (Jump x) -- 5
+-- Pop -- 1
+-- Dup -- 1
+-- (Call x) -- 2
+-- Return -- 1
+
 
 data MyElement = MyInt Int | MyString String | MyChar Char deriving (Show)
 
@@ -59,10 +62,19 @@ compareOpCall 61 (x:y:xs) = trace ("x = " ++ show x ++ " == y = " ++ show y) ((i
 compareOpCall _ stack = trace ("stack = " ++ show stack) stack
 
 
+bytesToInt :: [Word8] -> Int
+bytesToInt bytes =
+    fromIntegral (byte 0) +
+    (fromIntegral (byte 1) `shiftL` 8) +
+    (fromIntegral (byte 2) `shiftL` 16) +
+    (fromIntegral (byte 3) `shiftL` 24)
+  where
+    byte n = genericTake 4 bytes !! n
+
 --           opcode   values    stack     PC    (new_stack, PC)
 evalValue :: Word8 -> [Word8] -> [Int] -> Int -> ([Int], Int)
 -- * OK
-evalValue 0x01 values stack pc = trace ("LOAD_CONST "    ++ show (word8ToInt (values !! 0))) ((word8ToInt (values !! 0) : stack), pc + 2)
+evalValue 0x01 values stack pc = trace ("LOAD_CONST "    ++ show (bytesToInt values)) (((bytesToInt values) : stack), pc + 5)
 -- TODO
 evalValue 0x02 values stack pc = trace ("LOAD_VAR "      ++ show (word8ToInt (values !! 0))) ((word8ToInt (values !! 0) : stack), pc + 2)
 -- TODO
@@ -74,11 +86,11 @@ evalValue 0x05 values stack pc = trace ("UNARY_OP "      ++ show (word8ToInt (va
 -- * OK
 evalValue 0x06 values stack pc = trace ("COMPARE_OP "    ++ show (word8ToInt (values !! 0))) (compareOpCall (values !! 0) stack, pc + 2)
 -- * OK
-evalValue 0x07 values stack pc = trace ("JUMP_IF_TRUE "  ++ show (word8ToInt (values !! 0))) (if (stack !! 0) /= 0 then (stack, word8ToInt (values !! 0)) else (stack, pc + 2))
+evalValue 0x07 values stack pc = trace ("JUMP_IF_TRUE "  ++ show (bytesToInt values)) (if (stack !! 0) /= 0 then (stack, (bytesToInt values)) else (stack, pc + 5))
 -- * OK
-evalValue 0x08 values stack pc = trace ("JUMP_IF_FALSE " ++ show (word8ToInt (values !! 0))) (if (stack !! 0) == 0 then (stack, word8ToInt (values !! 0)) else (stack, pc + 2))
+evalValue 0x08 values stack pc = trace ("JUMP_IF_FALSE " ++ show (bytesToInt values)) (if (stack !! 0) == 0 then (stack, (bytesToInt values)) else (stack, pc + 5))
 -- * OK
-evalValue 0x09 values stack _ = trace ("JUMP "          ++ show (word8ToInt (values !! 0))) (stack, word8ToInt (values !! 0))
+evalValue 0x09 values stack _ = trace ("JUMP "          ++ show (bytesToInt values)) (stack, bytesToInt values)
 -- TODO, Pop the top value from the stack.
 evalValue 0x0A values stack pc = trace  "POP "                                               ((word8ToInt (values !! 0) : stack), pc + 1)
 -- TODO, Duplicate the top value on the stack.
@@ -87,18 +99,18 @@ evalValue 0x0B values stack pc = trace  "DUP "                                  
 evalValue 0x0C values stack pc = trace ("CALL "          ++ show (word8ToInt (values !! 0))) ((word8ToInt (values !! 0) : stack), pc + 2)
 -- TODO, return the value at the top of the stack to the caller.
 evalValue 0x0D _ stack _ = trace  "RETURN "                                            (stack, -1)
-evalValue _ _ _ _ = trace "Unknown opcode" ([], 0)
+evalValue a b c d = trace ("Unknown opcode: " ++ show a ++ " | values: " ++ show b ++ " | stack: " ++ show c ++ " | pc: " ++ show d) ([], -1)
 
 -- ? we have two bytecodes lists because if we move forward in the list, we can't go back
 --              bytecodes  bytecodes  stack      PC   (new_bytecodes, new_stack)
 evalEachValue :: [Word8] -> [Word8] -> [Int] -> Int -> ([Word8], [Int])
 evalEachValue _ [] stack _ = trace ("-- End of bytecodes Stack: " ++ show stack) ([], stack)
 evalEachValue bytecodes (x:xs) stack pc = do
-    let (new_stack, new_pc) = evalValue x xs stack pc
+    let (new_stack, new_pc) = trace ("evalValue executed with pc = " ++ show pc ++ " | opcode = " ++ show x) $ evalValue x xs stack pc
     if new_pc == -1 then (bytecodes, new_stack)
-    else if new_pc < pc then error "Invalid PC value"
     else
-        trace ("pc = " ++ show pc ++ " | " ++ show x ++ " | " ++ show new_stack ++ " | " ++ show (drop pc bytecodes)) $ evalEachValue bytecodes (drop pc bytecodes) new_stack new_pc
+        trace ("pc = " ++ show pc ++ " | opcode = " ++ show x ++ " | stack = " ++ show stack ++ " | new_stack = " ++ show new_stack ++ " | new_pc = " ++ show new_pc)
+        $ evalEachValue bytecodes (drop new_pc bytecodes) new_stack new_pc
 
 
 stringToWord8 :: String -> [Word8]
