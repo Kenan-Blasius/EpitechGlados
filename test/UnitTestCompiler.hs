9a910d4b5fdd52772ad124c6f5306467d9473766
Niels <91876336+NielsOuvrard@users.noreply.github.com>
2024-01-09 15:51:07 +0100

    😱 first unit test

--- test/UnitTestCompiler.hs
+++ test/UnitTestCompiler.hs
@@ -3,18 +3,22 @@ module UnitTestCompiler (
 ) where
 
 import Test.HUnit
-
-testCompileFunction :: Test
-testCompileFunction =
-    TestList
-    [
-        TestCase (assertEqual "compileFunction" (True) (True))
-    ]
+import Types
+import BytecodeToBinary
+import AstToBytecode
 
 testCompilerFunction :: Test
 testCompilerFunction =
     TestList
         [
-            TestLabel "compileFunction" testCompileFunction
+            TestLabel "compile, return 0" testCompiler
         ]
 
+testCompiler :: Test
+testCompiler =
+    TestList
+        [
+            let ast = AST [FunAST "main" DeadLeafAST (FunTypeAST (AST [IntTypeAST])) (AST [ReturnAST (AST [IntAST 0])])]
+            in let (_, bytecode, _) = astToBytecode' ast 0
+            in TestCase (assertEqual "compile, return 0" (bytecode) ([Jump 37,LoadConst 0,Return,Return]))
+        ]
