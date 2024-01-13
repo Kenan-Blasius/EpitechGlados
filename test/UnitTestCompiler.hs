0313948c41137984735ab370c1625cbacc4e6de0
Niels <91876336+NielsOuvrard@users.noreply.github.com>
2024-01-13 16:26:44 +0100

    ✨ clean stack tests

--- test/UnitTestCompiler.hs
+++ test/UnitTestCompiler.hs
@@ -23,18 +23,18 @@ testReturn :: Test
 testReturn =
     TestList
         [
-            let ast = AST [FunAST "main" (AST [IntTypeAST,SymbolAST "spain\127466\127480"]) (FunTypeAST (AST [IntTypeAST])) (ReturnAST (AST [IntAST 0]))]
+            let ast = AST []
             in let (_, bytecode, _) = astToBytecode' ast 0 []
-            in TestCase (assertEqual "compile, return 0" (bytecode) ([FunEntryPoint "main" IntType,StoreVarBefore "spain🇪🇸" IntType,LoadConst 0 IntType,Return,Return]))
+            in TestCase (assertEqual "compile, return 0" (bytecode) ([]))
         ]
 
 testAssignAST :: Test
 testAssignAST =
     TestList
         [
-            let ast = FunAST "main" DeadLeafAST (FunTypeAST (AST [IntTypeAST])) (AST [AssignAST (AST [IntTypeAST,SymbolAST "france\127467\127479"]) (AST [IntAST 0]),PlusEqualAST (AST [SymbolAST "france\127467\127479"]) (AST [IntAST 5]),ReturnAST (AST [SymbolAST "france\127467\127479"])])
+            let ast = AST []
             in let (_, bytecode, _) = astToBytecode' ast 0 []
-            in TestCase (assertEqual "compile, variable declaration" (bytecode) ([FunEntryPoint "main" IntType,LoadConst 0 IntType,StoreVarBefore "france🇫🇷" IntType,LoadVarBefore "france🇫🇷" UnknownType,LoadConst 5 IntType,BinaryOp "+",StoreVarBefore "france🇫🇷" UnknownType,LoadVarBefore "france🇫🇷" UnknownType,Return,Return]))
+            in TestCase (assertEqual "compile, variable declaration" (bytecode) ([]))
         ]
 
 testIfAST :: Test
@@ -50,16 +50,16 @@ testWhileAST :: Test
 testWhileAST =
     TestList
         [
-            let ast = FunAST "main" DeadLeafAST (FunTypeAST (AST [IntTypeAST])) (AST [AssignAST (AST [IntTypeAST,SymbolAST "indian\127470\127475"]) (AST [IntAST 15]),WhileAST (GreaterThanAST (AST [SymbolAST "indian\127470\127475"]) (AST [IntAST 0])) (DecrementAST (AST [SymbolAST "indian\127470\127475"])),ReturnAST (AST [SymbolAST "indian\127470\127475"])])
+            let ast = AST []
             in let (_, bytecode, _) = astToBytecode' ast 0 []
-            in TestCase (assertEqual "compile, while statement" (bytecode) ([FunEntryPoint "main" IntType,LoadConst 15 IntType,StoreVarBefore "indian🇮🇳" IntType,JumpRef 2,LoadVarBefore "indian🇮🇳" StringType,LoadConst 0 IntType,CompareOp ">",JumpIfFalseBefore 1,LoadVarBefore "indian🇮🇳" UnknownType,LoadConst 1 IntType,BinaryOp "-",StoreVarBefore "indian🇮🇳" UnknownType,JumpBefore 2,JumpRef 1,LoadVarBefore "indian🇮🇳" UnknownType,Return,Return]))
+            in TestCase (assertEqual "compile, while statement" (bytecode) ([]))
         ]
 
 testForAST :: Test
 testForAST =
     TestList
         [
-            let ast = FunAST "main" DeadLeafAST (FunTypeAST (AST [IntTypeAST])) (AST [AssignAST (AST [IntTypeAST,SymbolAST "mexico\127474\127485"]) (AST [IntAST 15]),ForAST (AssignAST (AST [IntTypeAST,SymbolAST "i"]) (AST [IntAST 0])) (LessThanAST (AST [SymbolAST "i"]) (AST [IntAST 5])) (IncrementAST (AST [SymbolAST "i"])) (PlusEqualAST (AST [SymbolAST "mexico\127474\127485"]) (AST [SymbolAST "i"])),ReturnAST (AST [SymbolAST "mexico\127474\127485"])])
+            let ast = AST []
             in let (_, bytecode, _) = astToBytecode' ast 0 []
             in TestCase (assertEqual "compile, for statement" (bytecode) ([]))
         ]
@@ -70,15 +70,15 @@ testFunAST =
         [
             let ast = AST []
             in let (_, bytecode, _) = astToBytecode' ast 0 []
-            in TestCase (assertEqual "compile, function declaration" (bytecode) ([FunEntryPoint "main" IntType,LoadConst 15 IntType,StoreVarBefore "mexico🇲🇽" IntType,LoadConst 0 IntType,StoreVarBefore "i" IntType,JumpRef 2,LoadVarBefore "i" StringType,LoadConst 5 IntType,CompareOp "<",JumpIfFalseBefore 1,LoadVarBefore "mexico🇲🇽" UnknownType,LoadVarBefore "i" UnknownType,BinaryOp "+",StoreVarBefore "mexico🇲🇽" UnknownType,LoadVarBefore "i" UnknownType,LoadConst 1 IntType,BinaryOp "+",StoreVarBefore "i" UnknownType,JumpBefore 2,JumpRef 1,LoadVarBefore "mexico🇲🇽" UnknownType,Return,Return]))
+            in TestCase (assertEqual "compile, function declaration" (bytecode) ([]))
         ]
 
 testFunCallAST :: Test
 testFunCallAST =
     TestList
         [
-            let ast = AST [FunAST "add" (AST [AST [IntTypeAST,SymbolAST "a"],AST [IntTypeAST,SymbolAST "b"]]) (FunTypeAST (AST [AST [IntTypeAST]])) (ReturnAST (PlusAST (AST [SymbolAST "a"]) (AST [SymbolAST "b"]))),FunAST "main" DeadLeafAST (FunTypeAST (AST [IntTypeAST])) (AST [AssignAST (AST [IntTypeAST,SymbolAST "south_corea\127472\127479"]) (AST [IntAST 7]),AssignAST (AST [IntTypeAST,SymbolAST "north_corea\127472\127477"]) (AST [IntAST 3]),AssignAST (AST [IntTypeAST,SymbolAST "corea"]) (AST [SymbolAST "add",AST [AST [SymbolAST "south_corea\127472\127479"],AST [SymbolAST "north_corea\127472\127477"]]]),ReturnAST (AST [SymbolAST "corea"])])]
+            let ast = AST []
             in let (_, bytecode, _) = astToBytecode' ast 0 []
-            in TestCase (assertEqual "compile, function call" (bytecode) ([FunEntryPoint "add" IntType,StoreVarBefore "a" IntType,StoreVarBefore "b" IntType,LoadVarBefore "a" UnknownType,LoadVarBefore "b" UnknownType,BinaryOp "+",Return,Return,FunEntryPoint "main" IntType,LoadConst 7 IntType,StoreVarBefore "south_corea🇰🇷" IntType,LoadConst 3 IntType,StoreVarBefore "north_corea🇰🇵" IntType,LoadVarBefore "south_corea🇰🇷" UnknownType,LoadVarBefore "north_corea🇰🇵" UnknownType,LoadPC,CallUserFun "add",StoreVarBefore "corea" IntType,LoadVarBefore "corea" UnknownType,Return,Return]))
+            in TestCase (assertEqual "compile, function call" (bytecode) ([]))
         ]
 
